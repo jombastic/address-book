14af901e5df9bf39a20b7fb58107d8b424982b57
Slavcho Mitrov <slavemitrov@yahoo.com>
2017-05-26 23:59:12 +0200

    Add buttons for removal of each contact and hide other contacts clicking to show one

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
     var numberType = $("#numberType").val();
     var number = $("#number").val();
 
-    $("#contacts").append("<div class='newContact' style='border:1px solid black;margin-top:10px;padding:5px;'>" + name + "<p style='display:none;'>" + numberType + "<br>" + number + "<br></p><button class='btn btn-primary nigga'>Delete</button></div>");
+    $("#contacts").append("<div class='newContact' style='border:1px solid black;margin-top:10px;padding:5px;'>" + name + "<p style='display:none;'>" + numberType + "<br>" + number + "<br><button class='btn btn-primary delete'>Delete</button></p></div>");
 
     event.preventDefault();
   });
   $("#contacts").on('click', '.newContact', function() {
-    $(this).find('p').toggle();
+    $("#contacts .newContact p").hide();
+    $(this).find('p').show();
   });
-  $("#contacts").on('click', '.nigga', function() {
-    $(this).closest("div").empty();
+  $("#contacts").on('click', '.delete', function() {
+    $(this).closest(".newContact").remove();
   });
 });
